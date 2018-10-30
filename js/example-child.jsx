9d7ffefc143655511917907798dd96c6d862bc74
soghband <soghband@gmail.com>
2018-10-30 08:49:20 +0700

    firstInit

---
+++ js/example-child.jsx
@@ -0,0 +1,9 @@
+'use strict';
+
+
+class ExampleChild extends React.Component {
+	render() {
+		return <h2>ExampleChild</h2>;
+	}
+}
+
---
+++ js/example.jsx
@@ -0,0 +1,10 @@
+'use strict';
+
+class Example extends React.Component {
+	render() {
+		return <div>
+			<h1>Example</h1>
+			<ExampleChild />
+		</div>;
+	}
+}
